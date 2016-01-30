97afa69cae46ab343abfe64103297f11629b8900
Luciano Tavares <lucianomt@gmail.com>
2016-01-30 10:29:29 -0200

    Adds daysBetween function - total days only

--- bizz.js
+++ bizz.js
@@ -5,9 +5,36 @@
 
   function define_Bizz() {
     var Bizz = {};
-    Bizz.greet = function() {
-      alert("yo!");
+
+    /**
+     * Calculate business days between two dates
+     *
+     * @param {Date} startDate
+     * @param {Date} endDate
+     * @param {Array} holidays
+     * @return {Number} days
+     */
+    Bizz.daysBetween = function(startDate, endDate, holidays) {
+      // TODO: check if startDate < endDate
+
+      return _daysBetween(startDate, endDate);
     }
+
+    function _daysBetween(startDate, endDate) {
+      // The number of milliseconds in one day
+      var ONE_DAY = 1000 * 60 * 60 * 24;
+
+      // Convert both dates to milliseconds
+      var startDate_ms = startDate.getTime();
+      var endDate_ms = endDate.getTime();
+
+      // Calculate the difference in milliseconds
+      var difference_ms = Math.abs(startDate_ms - endDate_ms);
+
+      // Convert back to days and return
+      return Math.round(difference_ms/ONE_DAY);
+    }
+
     return Bizz;
   }
 
